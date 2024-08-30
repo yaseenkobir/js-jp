924497787a9928e8fee6672ff36a267a0e555849
yaseenkobir <167666589+yaseenkobir@users.noreply.github.com>
2024-08-30 15:09:14 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,151 @@
+//ex1
+function textChange(){
+	document.getElementById('tc').innerHTML="Welcome to WWZ";
+}
+//ex2
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+//ex3
+function bulbOn(){
+	document.getElementById('bulb').src='images/on.gif';
+}
+function bulbOff(){
+	document.getElementById('bulb').src='images/off.gif';
+}
+//ex4
+function Smile(){
+     document.getElementById('logo').
+     src='images/smile.png';
+}
+function Sad(){
+     document.getElementById('logo').src='images/sad.png';
+}
+//ex5
+function changeCss(){
+	document.getElementById('cc').style.color="blue";
+}
+//ex6
+function textShow(){
+     document.getElementById('tsh').style.display="block";
+}
+function textHide(){
+      document.getElementById('tsh').style.display="none";
+}
+//ex7
+function fontSize(){
+	document.getElementById('fz').style.fontSize="50px";
+}
+//ex8
+function sitDown(){
+     document.getElementById('man').
+     src='images/sitdown.png';
+}
+function standUp(){
+     document.getElementById('man').src='images/stanup.png';
+}
+//ex9
+function demoResult(){
+	document.getElementById('dr').innerHTML=5+5;
+}
+//ex10
+function demoResult1(){
+	document.getElementById('dr1').innerHTML=10.50;
+}
+//ex11
+function showValue(){
+	document.getElementById('ex').innerHTML=x=6;
+}
+//ex12
+function resultShow(){
+	document.getElementById('rs').innerHTML=(5 + 6) * 10;
+}
+//ex13
+function resultShow1(){
+	document.getElementById('rs1').innerHTML=5 * 10;
+}
+//ex14
+function resultShow2(){
+	document.getElementById('rs2').innerHTML=x=5 * 20;
+}
+//ex15
+function resultShow3(){
+	document.getElementById('rs3').innerHTML=x=5*50;
+}
+//ex16
+function changeBgc(){
+	document.getElementById('fbcg').style.backgroundColor="teal";
+}
+//ex17
+function picShow(){
+	document.getElementById('sc').style.display= 'block';
+}
+function picHide(){
+	
+	document.getElementById('sc').style.display= 'none';
+}
+//ex18
+function show(){
+	document.getElementById('scbs').style.backgroundColor='red';
+}
+
+function hide(){
+	
+	document.getElementById('scbs').style.backgroundColor= 'transparent';
+}
+//ex19
+function changeBorder(){
+	document.getElementById('cb').style.border= '5px solid black';
+}
+//ex20
+function bShow(){
+	document.getElementById('bb').style.border='5px dashed red';
+}
+function bHide(){
+	
+	document.getElementById('bb').style.border= 'none';
+}
+//ex21
+function alignItem(){
+	document.getElementById('ai').style.textAlign= 'center';
+}
+//ex22
+function bcgImg(){
+	document.getElementById('bimg').style.backgroundImage= "url('images/g2.png')";
+}
+//ex23
+function ca(){
+	document.getElementById("myDIV").style.animation = "mynewmove 4s 2";
+}
+//ex24
+function cda(){
+	document.getElementById("mDiv").style.animationDelay = "3s";
+}
+//ex25
+function bcgClip(){
+	document.getElementById("bclip").style.backgroundClip = "content-box";
+}
+//ex26
+function bcgPos(){
+	document.getElementById("bmg").style.backgroundPosition="top right"; 
+}
+//ex27
+function bcgSize(){
+	document.getElementById("bs").style.backgroundSize = "60px 120px";
+}
+//ex28
+function myFunction(){
+	document.getElementById("bf").style.backfaceVisibility = "visible";
+}
+function myFunction(){
+	document.getElementById("bf").style.backfaceVisibility = "hidden";
+}
+//ex29
+function changeBot(){
+	document.getElementById('cbb').style.borderBottom = '2px  solid red';
+}
+//ex30
+function changeBor(){
+	document.getElementById('cbbr').style.borderBottomLeftRadius = "25px";
+}
+//ex32
